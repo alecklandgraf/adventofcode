8c69cc2ee378049b26b4a12745938a76486942f9
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-12-23 19:53:25 -0800

    Update day6.py

--- day6.py
+++ day6.py
@@ -16,7 +16,7 @@ def turn_on(grid, start, end):
     """returns grid with lights turned on from start tuple (x1, y1) to end tuple (x2, y2)"""
     start_x, start_y = start
     end_x, end_y = end
-    grid[start_x:end_x+1, start_y:end_y+1] = True
+    grid[start_x:end_x + 1, start_y:end_y + 1] = True
     return grid
 
 
@@ -24,12 +24,13 @@ def turn_off(grid, start, end):
     """returns grid with lights turned off from start tuple (x1, y1) to end tuple (x2, y2)"""
     start_x, start_y = start
     end_x, end_y = end
-    grid[start_x:end_x+1, start_y:end_y+1] = False
+    grid[start_x:end_x + 1, start_y:end_y + 1] = False
     return grid
 
 
 def toggle(grid, start, end):
+    """returns a grid of the with the cells toggled from start tuple (x1, y1) to end tuple (x2, y2)"""
     start_x, start_y = start
     end_x, end_y = end
-    grid[start_x:end_x+1, start_y:end_y+1] = np.invert(grid[start_x:end_x+1, start_y:end_y+1])
+    grid[start_x:end_x + 1, start_y:end_y + 1] = np.invert(grid[start_x:end_x + 1, start_y:end_y + 1])
     return grid
